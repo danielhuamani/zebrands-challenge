d5a6311a967bbcc3ed8d40eb6461e1dd44d90ac2
daniel huamani <danielhuamani15@gmail.com>
2024-07-15 16:55:24 -0500

    added models channels and creation product to channels

--- src/apps/products/urls.py
+++ src/apps/products/urls.py
@@ -1,8 +1,9 @@
 from rest_framework import routers
-from apps.products.apis import BrandViewSet, ProductViewSet
+from apps.products.apis import BrandViewSet, ProductViewSet, ChannelViewSet
 
 router = routers.SimpleRouter()
 router.register(r'brands', BrandViewSet)
 router.register(r'products', ProductViewSet)
+router.register(r'channels', ChannelViewSet)
 
 urlpatterns = router.urls
